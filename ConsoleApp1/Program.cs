580d3787f8efd8e33235eed9a7a27925aff5aefb
bende-r <br242424@mail.ru>
2024-07-12 10:13:12 +0300

    Изменение структуры

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -7,36 +9,81 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string chars = "1234567890";
-            Process process;
+            Console.WriteLine("Введите путь к файлу архива (например, d:\\r\\12345.7z):");
+            string archivePath = Console.ReadLine();
+
+            Console.WriteLine("Введите путь для распаковки (например, d:\\r\\r):");
+            string outputPath = Console.ReadLine();
+
+            string chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int maxLength = 4; // Максимальная длина комбинации
+
+            for (int length = 1; length <= maxLength; length++)
+                GenerateCombinations(chars, new StringBuilder(), length, archivePath, outputPath);
+        }
+
+        static void GenerateCombinations(string chars, StringBuilder current, int maxLength, string archivePath, string outputPath)
+        {
+            if (current.Length == maxLength)
+            {
+                StartProcess(current.ToString(), archivePath, outputPath);
+                Console.WriteLine(current.ToString());
+                return;
+            }
 
             for (int i = 0; i < chars.Length; i++)
-                for (int j = 1; j < chars.Length; j++)
-                    for (int k = 0; k < chars.Length; k++)
-                        for (int o = 0; o < chars.Length; o++)
-                        {
-                            process = new Process
-                            {
-                                StartInfo = new ProcessStartInfo
-                                {
-                                    FileName = "cmd.exe",
-                                    RedirectStandardInput = true,
-                                    UseShellExecute = false
-                                }
-                            };
+            {
+                current.Append(chars[i]);
+                GenerateCombinations(chars, current, maxLength, archivePath, outputPath);
+                current.Length--; // Удаление последнего символа
+            }
+        }
 
-                            process.Start();
+        static void StartProcess(string ps, string archivePath, string outputPath)
+        {
+            string commands = $"\"C:\\Program Files\\7-Zip\\7z.exe \" -p" + ps + " x " + archivePath + " -o" + outputPath + " && taskkill /f /im \"Consoleapp1.exe\"  || RD /s/q d:\\r\\r ";
 
-                            string ps = $"{chars[i]}{chars[j]}{chars[k]}{chars[o]}";
-                            string commands = "\"C:\\Program Files\\7-Zip\\7z.exe \" -p" + ps + " x d:\\r\\364901.7z -od:\\r\\r  && taskkill /f /im \"Consoleapp1.exe\"  || RD /s/q d:\\r\\r ";
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true // скрыть окно консоли
+                }
+            };
 
-                            using (StreamWriter pWriter = process.StandardInput)
-                            {
-                                if (pWriter.BaseStream.CanWrite)
-                                    pWriter.WriteLine(commands);
-                            }
-                            process.WaitForExit();
-                        }
+            process.Start();
+
+            using (StreamWriter pWriter = process.StandardInput)
+            {
+                if (pWriter.BaseStream.CanWrite)
+                {
+                    pWriter.WriteLine(commands);
+                    pWriter.Close();
+                }
+            }
+
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            // Фильтрация вывода
+            FilterOutput(result);
+
+        }
+
+        static void FilterOutput(string output)
+        {
+            // Пример фильтрации: выводим только строки, содержащие "Extracting"
+            using (StringReader reader = new StringReader(output))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    if (line.Contains("Extracting"))
+                        Console.WriteLine(line);
+            }
         }
     }
 }
